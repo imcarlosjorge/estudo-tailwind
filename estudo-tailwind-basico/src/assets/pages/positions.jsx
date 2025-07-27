b2a33468df12dc0ccebf872ec2c2d2c6ef7110d8
Carlos Jorge <carlosjorgeprofissional@gmail.com>
2025-07-27 20:01:18 -0300

    Positions

--- estudo-tailwind-basico/src/assets/pages/positions.jsx
+++ estudo-tailwind-basico/src/assets/pages/positions.jsx
@@ -1,7 +1,60 @@
 export const Positions = () => {
-    return(
-        <>
-            <h1>Positions</h1>
-        </>
-    )
+  return (
+    <div className="w-[100%] h-[1500px]">
+      <h1>Positions</h1>
+
+      {/* botão de ajuda */}
+      <button
+        className="
+      bg-yellow-400 
+      px-[20px] 
+      py-[10px] 
+      rounded-[5px] 
+      font-bold 
+      cursor-pointer
+      fixed bottom-7 right-7"
+      >
+        Botão Fixado
+      </button>
+      
+      <button
+        className="
+      bg-red-400 
+      px-[20px] 
+      py-[10px] 
+      rounded-[5px] 
+      font-bold 
+      cursor-pointer
+      sticky top-0 z-50"
+      >
+        Botão Sticky
+      </button>
+
+      <button
+        className="
+      bg-red-400 
+      px-[20px] 
+      py-[10px] 
+      rounded-[5px] 
+      font-bold 
+      cursor-pointer
+      relative top-[100px] right-[-700px]"
+      >
+        Botão Relativo
+      </button>
+
+      <button
+        className="
+      bg-red-400 
+      px-[20px] 
+      py-[10px] 
+      rounded-[5px] 
+      font-bold 
+      cursor-pointer
+      absolute top-0 right-0"
+      >
+        Botão Absolute
+      </button>
+    </div>
+  );
 };
